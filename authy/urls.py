ea01ec503bd3770cf3f225ff8a3068874237cc3b
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-12-04 23:16:05 +0600

    authentication urls

--- authy/urls.py
+++ authy/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('signup/', SignUp, name='signup'),
     path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', authViews.LogOutView.as_view(), {'next_page': 'login'}, name='logout'),
+    path('changepassword/', PasswordChange, name='change-password'),
+    path('changepassword/done', PasswordChangeDone, name='change-password-done'),
+    path('passwordreset/', authViews.PasswordResetView.as_view(), name='password_reset'),
+    path('passwordreset/done', authViews.PasswordResetDone.as_view(), name='password_reset_done'),
+    path('passwordrset/<uidb64>/token/', authViews.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('passwordreset/complete', authViews.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
